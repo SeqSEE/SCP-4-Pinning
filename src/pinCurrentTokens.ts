4504711e1483f0741da087c625c8da2c1d9b4ee5
SeqSEE <SeqSEE@users.noreply.github.com>
2023-09-14 03:26:38 -0400

    get all collections

--- src/pinCurrentTokens.ts
+++ src/pinCurrentTokens.ts
@@ -18,66 +18,62 @@ export const pinCurrentTokens = async () => {
   let pins = BigInt(0);
   for (const header of headers) {
     const {index, contract, collectionName, totalTXs} = header;
-    if (index === 46 || index === 109) {
-      const collection = await provider.getCollection(contract);
-      if (Object.keys(collection).length > 0 || totalTXs > 0) {
-        console.log(`Fetched collection in ${Date.now() - startTime}ms`);
-      } else {
-        console.log(`Skipping ${collectionName} which has no tokens`);
-      }
-      let detectedFileType;
-      const requested = new Map<string, boolean>();
-      const exists = new Map<string, boolean>();
-      for (const token of collection) {
-        if (
-          requested.get(token.imgUrl) != true &&
-          exists.get(token.imgUrl) == undefined
-        ) {
+    const collection = await provider.getCollection(contract);
+    if (Object.keys(collection).length > 0 || totalTXs > 0) {
+      console.log(`Fetched collection in ${Date.now() - startTime}ms`);
+    } else {
+      console.log(`Skipping ${collectionName} which has no tokens`);
+    }
+    let detectedFileType;
+    const requested = new Map<string, boolean>();
+    const exists = new Map<string, boolean>();
+    for (const token of collection) {
+      if (
+        requested.get(token.imgUrl) != true &&
+        exists.get(token.imgUrl) == undefined
+      ) {
+        try {
+          console.log(`fetching ${collectionName}:${token.imgUrl}`);
+          const data = Buffer.from(
+            (
+              await axios.get(`${process.env.IPFS_GW}/ipfs/${token.imgUrl}`, {
+                timeout: 300000,
+              })
+            ).data
+          );
+          requested.set(token.imgUrl, true);
+          exists.set(token.imgUrl, true);
+          // Detect the MIME type of the file
+          detectedFileType = await fileTypeFromBuffer(data);
+        } catch (e) {
+          //console.log(e);
+          exists.set(token.imgUrl, false);
+          console.log(`Failed to fetch ${token.imgUrl}`);
+        }
+        if (exists.get(token.imgUrl) === true) {
           try {
-            console.log(`fetching ${collectionName}:${token.imgUrl}`);
-            const data = Buffer.from(
-              (
-                await axios.get(`${process.env.IPFS_GW}/ipfs/${token.imgUrl}`, {
-                  timeout: 300000,
-                })
-              ).data
+            await axios.post(
+              `${process.env.IPFS_API}/api/v0/files/cp?arg=/ipfs/${
+                token.imgUrl
+              }&arg=/${token.imgUrl}${
+                detectedFileType ? `.${detectedFileType.ext}` : ``
+              }`,
+              {timeout: 300000}
             );
-            requested.set(token.imgUrl, true);
-            exists.set(token.imgUrl, true);
-            // Detect the MIME type of the file
-            detectedFileType = await fileTypeFromBuffer(data);
           } catch (e) {
             //console.log(e);
-            exists.set(token.imgUrl, false);
-            console.log(`Failed to fetch ${token.imgUrl}`);
+            console.log(`Error while copying ${token.imgUrl}`);
           }
-          if (exists.get(token.imgUrl) === true) {
-            try {
-              await axios.post(
-                `${process.env.IPFS_API}/api/v0/files/cp?arg=/ipfs/${
-                  token.imgUrl
-                }&arg=/${token.imgUrl}${
-                  detectedFileType ? `.${detectedFileType.ext}` : ``
-                }`,
-                {timeout: 300000}
-              );
-            } catch (e) {
-              //console.log(e);
-              console.log(`Error while copying ${token.imgUrl}`);
-            }
-            try {
-              await addPin(token.imgUrl);
-              pins++;
-            } catch (e) {
-              //console.log(e);
-              console.log(`Error while pinning ${token.imgUrl}`);
-            }
+          try {
+            await addPin(token.imgUrl);
+            pins++;
+          } catch (e) {
+            //console.log(e);
+            console.log(`Error while pinning ${token.imgUrl}`);
           }
-        } else {
-          console.log(
-            `Skipping ${collectionName}:${token.id} already requested`
-          );
         }
+      } else {
+        console.log(`Skipping ${collectionName}:${token.id} already requested`);
       }
     }
   }
