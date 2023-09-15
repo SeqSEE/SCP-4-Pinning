61c3572e4a47b329315b5c5b3588adabd64b94f4
SeqSEE <SeqSEE@users.noreply.github.com>
2023-09-15 03:29:19 -0400

    add try catch in the collection fetching

--- src/APIProvider.ts
+++ src/APIProvider.ts
@@ -8,19 +8,29 @@ export class APIProvider implements Provider {
   async getAllCollectionHeaders(): Promise<any[]> {
     let headers: any[] = [];
     const url = `${this.baseURL}/getallcollectionheaders`;
-    const response = await axios.get(url);
-    headers = response.data ? response.data : [];
+    try {
+      const response = await axios.get(url);
+      headers = response.data ? response.data : [];
+    } catch (e) {
+      console.log('Failed to get all collection headers');
+    }
     return headers;
   }
 
   async getCollection(contract: string): Promise<any[]> {
     let nfts: any[] = [];
-    if (!contract.match(Bytes32)) {
-      throw new Error(`Malformed contract expected bytes32 got: '${contract}'`);
+    try {
+      if (!contract.match(Bytes32)) {
+        throw new Error(
+          `Malformed contract expected bytes32 got: '${contract}'`
+        );
+      }
+      const url = `${this.baseURL}/getcollection/${contract}`;
+      const response = await axios.get(url);
+      nfts = response.data.nfts ? response.data.nfts : [];
+    } catch (e) {
+      console.log(`Failed to get collection ${contract}`);
     }
-    const url = `${this.baseURL}/getcollection/${contract}`;
-    const response = await axios.get(url);
-    nfts = response.data.nfts ? response.data.nfts : [];
     return nfts;
   }
 }
